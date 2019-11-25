ed2fc86836c7c1f1f0a1b9b3236cf1d18a65a43b
Jake Garlick <jacobgarlick@msn.com>
2019-11-25 14:57:58 -0700

    began to develop app with 2 test users to confirm functionality

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -1,56 +1,37 @@
 import React from 'react';
-import { ChatManager, TokenProvider } from "@pusher/chatkit-client"
+import { ChatkitProvider, ChatManager, TokenProvider, withChatkit } from "@pusher/chatkit-client-react"
 import "./style.css";
 
 const instanceLocator = "v1:us1:dfcf9bdc-62b3-40fc-b6ff-57ebe9fbdd70";
 const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/dfcf9bdc-62b3-40fc-b6ff-57ebe9fbdd70/token";
 const username = "JakeG";
-const roomId = `4cfd39a9-6b51-4065-9c8b-cc1e67a8f8b9`
+const roomId = `4cfd39a9-6b51-4065-9c8b-cc1e67a8f8b9`;
+const userId = "JakeG";
+const tokenProvider = new TokenProvider({url: testToken});
 
-class MessageList extends React.Component {
 
-    state = {
-        messages: []
-    }
-
-    componentDidMount() {
-        const chatManager = new ChatManager({
-            instanceLocator: instanceLocator,
-            userId: "jakeDoe",
-            tokenProvider: new TokenProvider({
-                url: testToken
-            })
-        })
-        chatManager.connect().then(currentUser => {
-            currentUser.subscribeToRoom({
-                roomId: roomId,
-                hooks: {
-                    onNewMessage: message => {
-                        this.setState({ messages: [...this.state.messages, message]})
-                    }
-                }
-            })
-        })
-    }
-
-    render () {
+function MessageList() {
+    const WelcomeMessage = withChatkit(props => {
         return (
-            <ul className="message-list">
-                {this.state.messages.map(message => {
-                    return (
-                        <li key={message.id}>
-                            <div>
-                                {message.senderId}
-                            </div>
-                            <div>
-                                {message.text}
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div>
+                {props.chatkit.isLoading
+                ? "Connecting to Chatkit..."
+            : `Hello ${props.chatkit.currentUser.name}`}
+            </div>
         )
-    }
+    })
+
+    return (
+        <div className="message-list">
+            <ChatkitProvider
+            instanceLocator={instanceLocator}
+            tokenProvider={tokenProvider}
+            userId={userId}
+            >
+            <WelcomeMessage />
+            </ChatkitProvider>
+        </div>
+    )
 }
 
 export default MessageList;
