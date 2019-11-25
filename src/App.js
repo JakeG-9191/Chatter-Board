72cf97cd2843245fe14a39f6500445f35a0cb432
Jake Garlick <jacobgarlick@msn.com>
2019-11-25 13:13:55 -0700

    inital react application build, started to make changes to app component rendering

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dudData = [
+  {
+    senderID: "someone",
+    text: "this is a test"
+  }, {
+    senderID: "another guy",
+    text: "still working through"
+  }
+]
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div className="application">
+        <Title />
+        <MessageList />
+        <SendMessageFrom />
+      </div>
+    )
+  }
 }
 
 export default App;
